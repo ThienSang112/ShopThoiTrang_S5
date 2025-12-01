d56079adf9d29bd6989dbda0fc3017b75ebf6e03
Thanh Sang <thanhsang24022004@gmail.com>
2025-12-01 13:02:55 +0700

    Cap nhat code

--- FE_25/src/components/admin/khach-hang/index.vue
+++ FE_25/src/components/admin/khach-hang/index.vue
@@ -25,7 +25,7 @@
                                     <tr class="text-center">
                                         <th class="align-middler">1</th>
                                         <td class="align-middler">Đỗ Thiên Sáng</td>
-                                        <td class="align-middler">Ipone 17 pro max</td>
+                                        <td class="align-middler">Áo thun</td>
                                         <td class="align-middler">thanhsang24022004@gmail.com</td>
                                         <td class="align-middler">098765123</td>
                                         <td class="align-middler">45 Hoang Diệu</td>
@@ -41,7 +41,7 @@
                                     <tr class="text-center">
                                         <th class="align-middler">2</th>
                                         <td class="align-middler">Đỗ Thiên Sáng</td>
-                                        <td class="align-middler">Ipone 17 pro max</td>
+                                        <td class="align-middler">Quần jean</td>
                                         <td class="align-middler">thanhsang24022004@gmail.com</td>
                                         <td class="align-middler">098765123</td>
                                         <td class="align-middler">45 Hoang Diệu</td>
@@ -56,41 +56,9 @@
                                     </tr>
                                     <tr class="text-center">
                                         <th class="align-middler">3</th>
-                                        <td class="align-middler">Đỗ Thiên Sáng</td>
-                                        <td class="align-middler">Ipone 17 pro max</td>
-                                        <td class="align-middler">thanhsang24022004@gmail.com</td>
-                                        <td class="align-middler">098765123</td>
-                                        <td class="align-middler">45 Hoang Diệu</td>
-                                        <td class="align-middler">
-                                            <button class="btn btn-primary me-2">Hiển thị</button>
-                                            <button class="btn btn-warning">Ẩn</button>
-                                        </td>
-                                        <td class="align-middler">
-                                            <button class="btn btn-info me-2">Cập nhật</button>
-                                            <button class="btn btn-danger">Xóa</button>
-                                        </td>
-                                    </tr>
-                                    <tr class="text-center">
-                                        <th class="align-middler">4</th>
-                                        <td class="align-middler">Đỗ Thiên Sáng</td>
-                                        <td class="align-middler">Ipone 17 pro max</td>
-                                        <td class="align-middler">thanhsang24022004@gmail.com</td>
-                                        <td class="align-middler">098765123</td>
-                                        <td class="align-middler">45 Hoang Diệu</td>
-                                        <td class="align-middler">
-                                            <button class="btn btn-primary me-2">Hiển thị</button>
-                                            <button class="btn btn-warning">Ẩn</button>
-                                        </td>
-                                        <td class="align-middler">
-                                            <button class="btn btn-info me-2">Cập nhật</button>
-                                            <button class="btn btn-danger">Xóa</button>
-                                        </td>
-                                    </tr>
-                                    <tr class="text-center">
-                                        <th class="align-middler">5</th>
-                                        <td class="align-middler">Đỗ Thiên Sáng</td>
-                                        <td class="align-middler">Ipone 17 pro max</td>
-                                        <td class="align-middler">thanhsang24022004@gmail.com</td>
+                                        <td class="align-middler">Huỳnh Văn Hiệp</td>
+                                        <td class="align-middler">Áo sơ mi</td>
+                                        <td class="align-middler">hiephuynh24022004@gmail.com</td>
                                         <td class="align-middler">098765123</td>
                                         <td class="align-middler">45 Hoang Diệu</td>
                                         <td class="align-middler">
